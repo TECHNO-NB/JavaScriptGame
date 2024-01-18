5147d66d4499c5f3db0d25203ed3468eb88917e3
NareshBhattarai4 <technologyandapplication@gmail.com>
2024-01-18 11:46:17 +0545

    JavaScriptGame

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 var count = 0;
-let animation = setInterval(animate, 30);
 
 function stop() {
   count++;
@@ -9,14 +8,13 @@ function stop() {
   var slide = "slider" + (count + 1);
   var sliderAbove = document.getElementById(slide);
 
-
   var left = parseInt(
     window.getComputedStyle(currentSlider).getPropertyValue("left")
   );
   var width = parseInt(
     window.getComputedStyle(currentSlider).getPropertyValue("width")
   );
-  
+
   currentSlider.style.left = left + "px";
   document.querySelector("#slider1").classList.remove("animate");
 
@@ -25,11 +23,20 @@ function stop() {
   sliderAbove.classList.add("animate");
 
   var slide = "slider" + (count - 1);
-  var sliderBelow= document.getElementById(slide);
+  var sliderBelow = document.getElementById(slide);
 
- 
   var sliderBelowleft = parseInt(
     window.getComputedStyle(sliderBelow).getPropertyValue("left")
   );
+  var diffrence = left - sliderBelowleft;
+  var absDiffrence = Math.abs(diffrence);
+  var newWidth = (width - absDiffrence).toString().concat("px");
 
+  currentSlider.style.width = newWidth;
+  sliderAbove.style.width = newWidth;
+
+  if (diffrence > width || diffrence < -width) {
+    alert(`Score: ${count}`);
+    location.reload();
+  }
 }
